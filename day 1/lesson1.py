11f5e0ef9756312ac98a8d9c4f8abecc1fbd1c12
Marsaga29 <marsagishvililuka29@gmail.com>
2022-09-22 20:02:20 +0400

    first commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,16 @@
+from email.headerregistry import ContentTransferEncodingHeader
+
+
+name = "luka"
+surname = "marsagishvili"
+age = 18
+height = 190
+weight = 75
+job = "studing coding"
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(weight))
+
+
+print(name + " " + str(surname) + " " + str(age) + "  " + str(height) + " " + str(weight) + " " + (job))
---
+++ day 2/lesson 2.py
@@ -0,0 +1,5 @@
+name = "luka"
+age = 18 
+heigt = 190
+
+print("chemi saxelia {} asaki {} da simaghle {}".format(name, age, heigt) )
